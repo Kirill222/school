9247e4cab3b3398b1cf908e654e3c92aa0a487a2
Kirill Zlobin <jsdevzk@gmail.com>
2022-10-02 14:54:19 +0200

    Create styles and menu for mobile version

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ $(document).ready(function () {
   let menuItems = $(document).find('.menu .item')
   let pageWrapper = $(document).find('.pages')
 
+  let burgerBtn = $(document).find('.burger-button')
+  let closeBtn = $(document).find('.close')
+
+  let mobileMenu = $(document).find('.mobile-menu')
+
   console.log(pageWrapper)
 
   menuItems.on('click', function () {
@@ -10,8 +15,17 @@ $(document).ready(function () {
 
     $(this).closest('.menu').find('.active').removeClass('active')
     $(this).addClass('active')
+    mobileMenu.css('top', '-100vh')
 
     pageWrapper.find('.current-page').removeClass('current-page')
     pageWrapper.find(`[data-page=${pageNumber}]`).addClass('current-page')
+  })
+
+  burgerBtn.on('click', function () {
+    mobileMenu.css('top', '0')
+  })
+
+  closeBtn.on('click', function () {
+    mobileMenu.css('top', '-100vh')
   })
 })
